2a1dc267b381ae1c27ea83d86fea2d0b8e350103
Frank Dekker <fdekker@gmail.com>
2021-07-17 21:28:20 +0200

    Add extra parent child setter,getter tests

--- tests/Unit/Constraint/MethodPair/AbstractParentClass.php
+++ tests/Unit/Constraint/MethodPair/AbstractParentClass.php
@@ -10,8 +10,12 @@ abstract class AbstractParentClass extends AbstractDataClass
     /** @var bool */
     protected $assertParentMethod = true;
 
+    /** @var string|null */
+    private $value;
+
     public function setValueA(string $valueA): void
     {
+        $this->value = $valueA;
     }
 
     public function getValueB(): string
@@ -21,6 +25,7 @@ abstract class AbstractParentClass extends AbstractDataClass
 
     public function setItem(string $item): void
     {
+        $this->value = $item;
     }
 
     public function getItem(): string
