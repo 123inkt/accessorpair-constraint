93b6441502357486bdb1877abe7e50dfa0e77c91
Bram Leeda <bram@123inkt.nl>
2023-08-28 15:36:11 +0200

    Fix the support for array<key, value> syntax to also support iterators / classes. (#51)

---
+++ tests/Unit/Constraint/Typehint/data/All/DocComment/IteratorTypedFull.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace DigitalRevolution\AccessorPairConstraint\Tests\Unit\Constraint\Typehint\data\All\DocComment;
+
+use ArrayIterator;
+use DigitalRevolution\AccessorPairConstraint\Tests\Unit\Constraint\Typehint\DataInterface;
+use phpDocumentor\Reflection\Fqsen;
+use phpDocumentor\Reflection\Type;
+use phpDocumentor\Reflection\Types\Collection;
+use phpDocumentor\Reflection\Types\String_;
+
+class IteratorTypedFull implements DataInterface
+{
+    /**
+     * @param ArrayIterator<string, string> $param
+     *
+     * @return ArrayIterator<string, string>
+     */
+    public function testMethod(ArrayIterator $param): ArrayIterator
+    {
+        return $param;
+    }
+
+    public function getExpectedType(): Type
+    {
+        return new Collection(new Fqsen('\\' . ArrayIterator::class), new String_(), new String_());
+    }
+}
