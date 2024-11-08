b07647616105ac1892db4dee9fb140fffdef3eda
Anne-Gaëlle Kerros <anne@123inkt.nl>
2024-11-08 14:39:58 +0100

    B#188652 AccessorPair accept all array notations

---
+++ tests/Unit/Constraint/ValueProvider/Compound/ArrayShapeProviderTest.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace Constraint\ValueProvider\Compound;
+
+use DigitalRevolution\AccessorPairConstraint\Constraint\ValueProvider\Compound\ArrayShapeProvider;
+use DigitalRevolution\AccessorPairConstraint\Constraint\ValueProvider\Scalar\IntProvider;
+use DigitalRevolution\AccessorPairConstraint\Constraint\ValueProvider\Scalar\StringProvider;
+use DigitalRevolution\AccessorPairConstraint\Tests\Unit\Constraint\ValueProvider\AbstractValueProviderTestCase;
+use Exception;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(ArrayShapeProvider::class)]
+class ArrayShapeProviderTest extends AbstractValueProviderTestCase
+{
+    /**
+     * @throws Exception
+     */
+    public function testGetValues(): void
+    {
+        $valueProvider = new ArrayShapeProvider(['foo' => new IntProvider(), 'bar' => new StringProvider()]);
+        $values        = $valueProvider->getValues();
+
+        static::assertNotCount(0, $values);
+        foreach ($values as $value) {
+            static::assertCount(2, $value);
+            static::assertArrayHasKey('foo', $value);
+            static::assertIsInt($value['foo']);
+            static::assertArrayHasKey('bar', $value);
+            static::assertIsString($value['bar']);
+        }
+    }
+}
